e49708e3d1b5eccf5a5f19a4982ed568906b1396
reshmi-02 <reshmihash123@gmail.com>
2023-08-01 11:47:50 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,15 @@
+var but=document.querySelector("a");
+console.log(but);
+
+
+    //console.log(ques);
+    but.addEventListener("click",function(){
+        if(but.nextElementSibling.classList.contains("para")){
+            but.nextElementSibling.classList.remove("para");              
+        }
+
+
+        else{
+            but.nextElementSibling.classList.add("para");
+        }
+    })
